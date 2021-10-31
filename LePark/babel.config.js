453bf39f52e79d16474a6076835a937ea91a3d7e
angjustin <89179725+angjustin@users.noreply.github.com>
2021-10-31 18:09:12 +0800

    updated babel.config

--- LePark/babel.config.js
+++ LePark/babel.config.js
@@ -1,11 +1,12 @@
 module.exports = function(api) {
   api.cache(true);
 
-  const plugins = [
-    ["module:react-native-dotenv"]
-  ]
   return {
-    presets: ['babel-preset-expo'],
-    plugins
+    presets: [
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset',
+      'module:react-native-dotenv'
+  ],
+    plugins: ["module:react-native-dotenv"]
   };
 };

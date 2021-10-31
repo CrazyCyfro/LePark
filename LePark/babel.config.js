54b4821e97125b092f25437fe17a24d75efd94ae
angjustin <89179725+angjustin@users.noreply.github.com>
2021-10-31 18:13:38 +0800

    reverted babel

--- LePark/babel.config.js
+++ LePark/babel.config.js
@@ -1,12 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
 
+  const plugins = [
+    ["module:react-native-dotenv"]
+  ]
   return {
-    presets: [
-      'babel-preset-expo',
-      'module:metro-react-native-babel-preset',
-      'module:react-native-dotenv'
-  ],
-    plugins: ["module:react-native-dotenv"]
+    presets: ['babel-preset-expo'],
+    plugins
   };
 };
